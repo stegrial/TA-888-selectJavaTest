d958811431f72062093b3d5913190be4dde0bf7f
Grigoriy Stetsenko <stetsenko@softesis.com>
2019-06-19 14:58:02 +0300

    creating project

---
+++ src/test/java/exampleTest.java
@@ -0,0 +1,39 @@
+import io.trueautomation.client.driver.TrueAutomationDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.trueautomation.client.TrueAutomationHelper.ta;
+
+public class exampleTest {
+    private WebDriver driver;
+    private By dropedown = By.cssSelector(ta("main:dropedown", "#o_type"));
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new TrueAutomationDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @Test(description = "Using the 'select' method for drop-down lists.")
+    public void methodSelectTest() throws InterruptedException {
+        driver.get("https://dipservis.com.ua/cabinet");
+
+        Select element = new Select(driver.findElement(dropedown));
+        element.selectByValue("13");
+//        element.selectByVisibleText("Отчет");
+
+        Thread.sleep(2000);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+}
